9702bfc51d7e15f0e825de2b6a22b2de60047cb7
unknown <sujathajessy123@gmail.com>
2022-03-02 21:13:05 +0530

    assignment8

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -1,16 +1,17 @@
-var input_field = document.querySelector('input');
-var btn = document.querySelector('button');
-var todo_list = document.querySelector('div.todos');
-var count = 0;
-btn.addEventListener('click', function(){
-    var para = document.createElement('p');
-    para.setAttribute('key', count);
-    count += 1
-    para.innerHTML= input_field.value;
-    input_field.value = "";
-    todo_list.append(para);
-    para.addEventListener('click', function(){
-        todo_list.removeChild(para);
+var input_text=document.querySelector('input');
+var add_but=document.querySelector('button');
+var todo_data=document.querySelector('.todos');
+var count=0;
+add_but.addEventListener('click',function(){
+    var data=input_text.value
+    var para=document.createElement('p');
+    para.setAttribute('key',count);
+    para.append(data);
+    todo_data.append(para);
+    input_text.value=" ";
+    count+=1
+    para.addEventListener('click',function(){
+        para.remove()
+        count-=1
     })
-
 })
